bb18f8d27fe6d63b0eda303303cf354c576e3caa
anastasia <anastasia.aizman@gmail.com>
2019-10-30 10:58:37 -0400

    add - more icons

---
+++ frontend/src/components/icons/heptagon.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+var icon = require('vue-svgicon')
+icon.register({
+  'heptagon': {
+    width: 20,
+    height: 20,
+    viewBox: '0 0 20 20',
+    data: '<path pid="0" d="M9.688.625l7.573 3.647 1.871 8.196-5.241 6.573H5.484L.243 12.468l1.87-8.196z" _fill="#4A90E2" fill-rule="nonzero"/>'
+  }
+})
---
+++ frontend/src/components/icons/oval.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+var icon = require('vue-svgicon')
+icon.register({
+  'oval': {
+    width: 16,
+    height: 19,
+    viewBox: '0 0 16 19',
+    data: '<ellipse pid="0" cx="9.885" cy="160.14" rx="7.252" ry="8.958" transform="translate(-2 -151)" _fill="#D59914" fill-rule="nonzero"/>'
+  }
+})
