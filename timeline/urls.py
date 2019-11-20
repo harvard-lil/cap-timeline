a1081f187f0885a8482d9bee04cd1113dcbe87a2
anastasia <anastasia.aizman@gmail.com>
2019-11-19 23:36:40 -0500

    fix - missing slash was breaking everything, it turns out

--- timeline/urls.py
+++ timeline/urls.py
@@ -4,6 +4,7 @@ from django.views.generic.base import TemplateView
 from research import views
 
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
     path('data/events/', views.events),
     path('data/events/<int:event_id>', views.event),
@@ -11,6 +12,5 @@ urlpatterns = [
     path('data/groups/', views.groups),
     path('data/groups-by-region/', views.groups_by_region),
     path('data/year-settings/', views.year_settings),
-    path('', views.index),
-    re_path(r'^.*$', views.index),
+    re_path(r'^.*/$', views.index)
 ]
