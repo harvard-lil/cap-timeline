1552528c1553c741622474381919094c4c001c2f
anastasia <anastasia.aizman@gmail.com>
2019-11-21 15:13:15 -0500

    update - years respond to toggle changes

--- static/src/components/TimelineView.vue
+++ static/src/components/TimelineView.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="timeline">
     <div class="container-wrapper">
-      <template v-for="(year,idx) in years">
-        <div class="year-label" :class="'col-'+getIdx(idx)">{{year}}</div>
-        <template v-for="event in events[year]">
+      <template v-for="(year,idx) in maxYear-minYear">
+        <div class="year-label"
+             v-bind:key="year+minYear"
+             :class="'col-'+getIdx(idx)"> {{year+minYear}}
+        </div>
+        <template v-for="event in events[year+minYear]">
           <div class="event-container"
+               v-bind:key="event.id"
                :class="['event-type-'+event.type, 'span' + getLength(event), 'col-'+getIdx(idx)]">
             <div class="event-type">{{event.type}}</div>
-            {{event.name}}
+            <div class="event-name">{{event.name}}</div>
+            <div class="event-description-short" v-html="event.description_short"></div>
           </div>
         </template>
       </template>
@@ -17,6 +22,7 @@
 
 <script>
   import Vue from 'vue';
+  import store from '../store';
 
   export default {
     name: "TimelineView",
@@ -41,7 +47,7 @@
               for (let i = 0; i < response.body.length; i++) {
                 let year = this.getYear(response.body[i].start_date);
                 if (year in this.events) {
-                  this.events[year].push(response.body[i])
+                  this.events[year].push(response.body[i]);
                   Vue.set(this.events, year, this.events[year])
                 } else {
                   Vue.set(this.events, year, [response.body[i]])
@@ -62,13 +68,17 @@
       },
       getIdx(idx) {
         return idx + 1;
+      },
+    },
+    computed: {
+      minYear() {
+        return store.getters.getMinYear;
+      },
+      maxYear() {
+        return store.getters.getMaxYear;
       }
     },
-    beforeMount() {
-      for (const x of Array(20).keys()) {
-        this.years.push(x + 1905)
-      }
-    },
+
     created() {
       this.getData();
       this.getGroups();
