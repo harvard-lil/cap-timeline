bf555bf3203ca7da988156f7dd35e39dd0655e88
Anastasia Aizman <anastasia.aizman@gmail.com>
2020-01-07 16:01:46 -0500

    add - home view

--- static/router/index.js
+++ static/router/index.js
@@ -3,14 +3,20 @@ import Router from 'vue-router';
 import Events from '../src/components/Events';
 import EventView from '../src/components/EventView';
 import TimelineView from '../src/components/TimelineView';
+import HomeView from '../src/components/HomeView';
 
 Vue.use(Router);
 export default new Router({
   mode: 'history',
   routes: [
     {
-      path: '/:slug/',
+      path: '/',
       name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/:slug/',
+      name: 'timeline',
       component: TimelineView
     },
     {
@@ -23,11 +29,5 @@ export default new Router({
       name: 'eventview',
       component: EventView,
     },
-    {
-      path: '/:slug/timeline/',
-      name: 'timelineview',
-      component: TimelineView,
-    },
-
   ]
 });
---
+++ static/src/components/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>hello!</div>
+</template>
+
+<script>
+  export default {
+    name: "homeview"
+  }
+</script>
+
+<style scoped>
+
+</style>
