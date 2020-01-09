0b15c08452de624e6ce73bb0bbe5bfd6e7844ecf
Anastasia Aizman <anastasia.aizman@gmail.com>
2020-01-09 17:09:09 -0500

    add - sort-of-dynamic title setting

--- static/src/main.js
+++ static/src/main.js
@@ -4,11 +4,21 @@ import Resource from 'vue-resource';
 import App from './App.vue'
 import router from '../router';
 import './assets/css/styles.scss';
+
 import store from './store'
+
 Vue.use(Resource);
 Vue.use(VueSVGIcon);
 
 Vue.config.productionTip = false;
+router.beforeEach((to, from, next) => {
+  if (to.params.slug) {
+    document.title = "Timeline: " + to.params.slug;
+  } else {
+    document.title = to.meta.title;
+  }
+  next()
+});
 
 new Vue({
   router: router,
