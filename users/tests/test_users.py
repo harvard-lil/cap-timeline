95e8904fb40d645b02dcde2ac809363d364f84cc
Anastasia Aizman <anastasia.aizman@gmail.com>
2020-01-10 10:24:05 -0500

    bump - circle

--- users/tests/test_users.py
+++ users/tests/test_users.py
@@ -2,11 +2,12 @@ import pytest
 from test_fixtures.fixtures import *  # noqa
 
 
+@pytest.mark.django_db
 def test_user(user):
     assert user.email
 
 
+@pytest.mark.django_db
 def test_superuser(superuser):
     assert superuser.is_staff
     assert superuser.is_superuser
-
