c91947d3502972d6dd0020c6449012bcc1149880
Anastasia Aizman <anastasia.aizman@gmail.com>
2020-01-23 14:10:37 -0500

    fix - remove "migrants" setting in groups

--- static/src/components/Group.vue
+++ static/src/components/Group.vue
@@ -1,13 +1,13 @@
 <template>
-  <li class="list-inline-item group" :title="fullName + ' migrants'">
+  <li class="list-inline-item group" :title="fullName">
     <span class="group-name" :class="{active: status}">
       <svgicon :icon="symbolTranslation[slug]"
-               :title="fullName + ' migrants'"
+               :title="fullName"
                :class="'group-symbol group-name-' + slug + ' symbol-' + symbolTranslation[slug]"
                width="13" height="13">
       </svgicon>
     </span>
-    <span v-if="showName" class="group-name">{{fullName}} migrants</span>
+    <span v-if="showName" class="group-name">{{fullName}}</span>
   </li>
 </template>
 <script>
